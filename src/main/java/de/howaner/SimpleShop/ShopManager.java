5c15187e9b0eda88bba6d9719f316c6a53e3677d
Howaner <franzi.moos@googlemail.com>
2013-08-31 17:44:09 +0200

    Add Manager Class

---
+++ src/main/java/de/howaner/SimpleShop/ShopManager.java
@@ -0,0 +1,18 @@
+package de.howaner.SimpleShop;
+
+public class ShopManager {
+	public SimpleShopPlugin p;
+	
+	public ShopManager(SimpleShopPlugin plugin) {
+		this.p = plugin;
+	}
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+}
--- src/main/java/de/howaner/SimpleShop/SimpleShopPlugin.java
+++ src/main/java/de/howaner/SimpleShop/SimpleShopPlugin.java
@@ -1,15 +1,33 @@
 package de.howaner.SimpleShop;
 
+import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleShopPlugin extends JavaPlugin {
+	public static Logger log;
+	private static SimpleShopPlugin instance;
+	private static ShopManager manager;
     
+	@Override
+	public void onLoad() {
+		log = this.getLogger();
+		instance = this;
+		manager = new ShopManager(this);
+	}
+	
     @Override
-    public void onEnable(){
-        
+    public void onEnable() {
+        if (log == null) log = this.getLogger();
+		if (instance == null) instance = this;
+		if (manager == null) manager = new ShopManager(this);
+		manager.onEnable();
+		log.info("Plugin enabled!");
     }
+	
     @Override 
-    public void onDisable(){
-        
+    public void onDisable() {
+        manager.onDisable();
+		log.info("Plugin disabled!");
     }
+	
 }
