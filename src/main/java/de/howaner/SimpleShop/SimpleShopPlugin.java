f81533b64540b0ce9752e177822307fc211adced
Jens <Jens@Jens-PC.fritz.box>
2013-08-31 12:25:58 +0200

    Add onEnable() and onDisable()

--- src/main/java/de/howaner/SimpleShop/SimpleShopPlugin.java
+++ src/main/java/de/howaner/SimpleShop/SimpleShopPlugin.java
@@ -1,5 +1,15 @@
 package de.howaner.SimpleShop;
 
-public class SimpleShopPlugin {
-	
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleShopPlugin extends JavaPlugin {
+    
+    @Override
+    public void onEnable(){
+        
+    }
+    @Override 
+    public void onDisable(){
+        
+    }
 }
